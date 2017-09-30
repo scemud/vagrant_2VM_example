57c1d21423d5536bf98d2223247c28948be8ad04
Barrett LaFrance <lghrants@gmail.com>
2017-09-30 10:35:56 -0500

    feat: add cli timings; can list docker images installed

--- cli.js
+++ cli.js
@@ -1,5 +1,15 @@
-var proc = require('child_process');
 var vorpal = require('vorpal')();
+var proc = require('child_process');
+var dockerode = require('dockerode');
+var Readable = require('stream').Readable;
+var JSONStream = require('JSONStream');
+var fs = require('fs');
+var moment = require('moment');
+var _ = require('lodash');
+
+var mongoTag = 'mongo:2.2.7'
+var logFile = 'cli.log';
+var started = new moment();
 
 function shellout(cmd) {
     return new Promise(function(resolve, reject) {
@@ -10,12 +20,56 @@ function shellout(cmd) {
     });
 }
 
+function clearLog() { fs.truncateSync(logFile, 0); }
+
+function fmt(msg) { return ` [cli] ${msg}`; }
+
+function outputTiming() {
+    let diff = new moment().diff(started, 'milliseconds');
+    let duration = moment.utc(diff).format('HH:mm:ss.SS');
+    vorpal.log(fmt(`done! (${duration})\n`));
+}
+
+var docker = new dockerode();
+docker.pullImage = (tag) => {
+    vorpal.log(fmt(`pulling ${tag}`));
+
+    return new Promise((resolve, reject) => {
+        // TODO: look into how to NOT pull if image already exists..?
+        //   if-else in Promises?  Streams from RxJs?
+        // docker.listImages(function(err, images){
+        //     let tags = _.flatMap(images, (image) => {
+        //         return image.RepoTags;
+        //     });
+        //     vorpal.log(tags);
+        // });
+
+        docker.pull(tag, (err, stream) => {
+            if (err) { reject(err); }
+            stream
+                .on('data', (chunk) => { /* process.stdout.write(chunk); */ })
+                .pipe(JSONStream.parse())
+                .pipe(JSONStream.stringify(false))
+                .pipe(fs.createWriteStream(logFile, {flags:'a'}))
+                .on('finish', resolve);
+        });
+    });
+}
+docker.imageExists = (tag) => {
+    docker.listImages(function(err, images){
+        let tags = _.flatMap(images, (image) => {
+            return image.RepoTags;
+        });
+        vorpal.log(tags);
+    });
+}
+
 vorpal
     .command('mongo status', 'Return status of local mongo server.')
     .option('-d, --dev', 'development instance')
     .option('-t, --test', 'test instance')
     .action(function(args, cb) {
-        this.log(args);
+
     });
 
 vorpal
@@ -23,9 +77,13 @@ vorpal
     .option('-d, --dev', 'development instance')
     .option('-t, --test', 'test instance')
     .action(function(args, cb) {
-        var self = this;
-        shellout("ls -la")
-            .then(function(stdout){ self.log(stdout); })
+        docker
+            .pullImage(mongoTag)
+            .then(() => {
+                
+            })
+            .catch((err) => { vorpal.log(err); })
+            .then(() => { outputTiming() });
     });
 
 vorpal
@@ -44,4 +102,5 @@ vorpal
         this.log(args);
     });
 
+clearLog();
 vorpal.parse(process.argv);
